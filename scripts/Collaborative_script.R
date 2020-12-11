33cf5be2d53976d559ba5939159240d12c4b8934
petronemilio <emilio.petrone@st.ib.unam.mx>
2020-12-10 19:34:41 -0600

    Definición de equipos y secciones.

--- scripts/Collaborative_script.R
+++ scripts/Collaborative_script.R
@@ -1,9 +1,11 @@
-
+#Cargando la base de datos
 traits <- read.csv("data/2020_11_12_Base.csv")
 names(traits) #revisar nombres#
 str(traits)
 
-#####exploratory analyses [XXXXXX]
+########## 
+#Equipo: Jess-Diego-Belén-EmmanuelMtz 
+#exploratory analyses [XXXXXX]
 #Checar existencia de outliers y corregir el problema
 #tipo de variables que tenemos, que el reconocimiento sea correcto
 #análisis de correlación bivariada de todas las variables
@@ -14,6 +16,8 @@ str(traits)
 #extraer datos de las dos fuentes y crear una nueva col donde se combinen para tener un único dato de long de hoja
 #explorar la variable final de long de hoja final
 
+#####Estadística descriptiva. Equipo: Claudia, Brenda y Angélica, Emmanuel García 
+#
 #####P1. ¿Qué tanto explica la longitud de la hoja la variación en diámetro  de los vasos en la base de un árbol, con y 
 #####  sin consideració de la altura? [XXXXX]
 #plot con Y:diámetro basal, X1:long de hoja, X2:altura
@@ -22,7 +26,8 @@ str(traits)
 #checar cumplimiento de supuestos en residuos de modelos ajustados
 #checar si tenemos colinealidad  
 
-
+######
+#Karla, Mariana, Karen, Iván
 #####P3. ¿Qué tanto explica la longitud de la hoja la variación en el diámetro  de los vasos en el ápice de un árbol, con y 
 #####sin consideración de la altura? [XXXXX]
 #plot con Y:diámetro apical, X1:long de hoja, X2:altura
