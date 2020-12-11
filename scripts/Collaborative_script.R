9434751047819b40a3da03f75ead886293784ca4
Karen-Vazquez <biokaren.vs@gmail.com>
2020-12-10 18:27:58 -0600

    comente quienes estamos en el equipo encargado de la pregunta 2

--- scripts/Collaborative_script.R
+++ scripts/Collaborative_script.R
@@ -3,19 +3,19 @@ traits <- read.csv("data/2020_11_12_Base.csv")
 names(traits) #revisar nombres#
 str(traits)
 
-#####exploratory analyses [XXXXXX]
+#####exploratory analyses [Eq. 1]
 #Checar existencia de outliers y corregir el problema
 #tipo de variables que tenemos, que el reconocimiento sea correcto
 #análisis de correlación bivariada de todas las variables
 #verificar los niveles de los factores, rangos de variación de vars continuas
 #datos faltantes, cuántos hay y cómo se están manejando
 
-#####creating final consensus variables
+#####creating final consensus variables [Eq.1]
 #extraer datos de las dos fuentes y crear una nueva col donde se combinen para tener un único dato de long de hoja
 #explorar la variable final de long de hoja final
 
 #####P1. ¿Qué tanto explica la longitud de la hoja la variación en diámetro  de los vasos en la base de un árbol, con y 
-#####  sin consideració de la altura? [XXXXX]
+#####  sin consideració de la altura? [Eq.3]
 #plot con Y:diámetro basal, X1:long de hoja, X2:altura
 #evaluar la necesidad de transformación de variables
 #modelo; ajustarlo
@@ -24,7 +24,7 @@ str(traits)
 
 
 #####P3. ¿Qué tanto explica la longitud de la hoja la variación en el diámetro  de los vasos en el ápice de un árbol, con y 
-#####sin consideración de la altura? [XXXXX]
+#####sin consideración de la altura? [Mariana, Iván, Karla, Karen]
 #plot con Y:diámetro apical, X1:long de hoja, X2:altura
 #evaluar la necesidad de transformación de variables
 #modelo; ajustarlo
