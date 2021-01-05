decc1689c3b24a8d95ff94c6a08fe4093ef4890e
Karen Vazquez <karenvazquez1@MacBookAirKaren.local>
2021-01-05 16:48:08 -0600

    Sugerencia de estructura del script

--- scripts/Collaborative_script.R
+++ scripts/Collaborative_script.R
@@ -1,55 +1,36 @@
-#Cargando la base de datos
-traits <- read.csv("data/2020-12-02_Base_curso_rasgos.csv",header = TRUE,sep=",")
-#Base de datos actualizada al 29-12-20
-str(traits)
-# Eliminar columnas que no usaremos
-data <- traits[,c(1:7,11:21)] 
-names(traits) #revisar nombres # Cambiar nombres
-colnames(data) <- c("order","family","genus","species.epithet",
-                    "stem.length.m", "VD.base.um","VD.tip.um.",
-                    "min.length.leaf.cm","max.length.leaf.cm",
-                    "min.length.petiole.cm", "max.length.petiole.cm",
-                    "min.length.blade.cm","max.lentgh.blade.cm",
-                    "min.width.blade.cm","max.width.blade.cm",
-                    "leaf.type","length.leaf.cm","width.leaf.cm") 
-# Crear columna con los nombres completos de las especies
-data["Species"] <- paste(data$genus, data$species.epithet, sep = "_")
-# En el último consenso acordamos que el tipo de hoja serviria para discriminar especies que no tuvieran datos
-#Subset eliminando aquellas filas que no presentan tipo de hoja. 
-data02 <- subset(data, leaf.type == "compound"|
-                   leaf.type=="simple")
 
-########## 
-#Equipo: Jess-Diego-Belén-EmmanuelMtz 
-#exploratory analyses [XXXXXX]
-#Checar existencia de outliers y corregir el problema
-#tipo de variables que tenemos, que el reconocimiento sea correcto
-#análisis de correlación bivariada de todas las variables
-#verificar los niveles de los factores, rangos de variación de vars continuas
+####### Comandos para limpiar y crear la variable consenso ##########
+#Checar existencia de outliers y ver si es dato original o hubo algún error en su captura
 #datos faltantes, cuántos hay y cómo se están manejando
+#crear la variable consenso final (extraer datos de las dos fuentes y crear una nueva col donde se combinen para tener un único dato de long de hoja)
 
-#####creating final consensus variables
-#extraer datos de las dos fuentes y crear una nueva col donde se combinen para tener un único dato de long de hoja
-#explorar la variable final de long de hoja final
+####### Análisis exploratorios de las variables que vamos a considerar en nuestros modelos #########
+#Checar existencia de outliers y ver si es dato original o hubo algún error en su captura
+#tipo de variables que tenemos, que el reconocimiento sea correcto factor, character, num, logical,
+#verificar los niveles de los factores, rangos de variación de variables continuas
+
+## Creo que estás dos secciones les corresponderian al Equipo 1: Jess-Diego-Belén-EmmanuelMtz ##
 
 
-#cargamos librerias
+
+############## Análisis estadísticos descriptivos ################
+## Creo que está sección le corresponde al Equipo 2: Claudia, Brenda y Angélica, Emmanuel García 
+
+############# Modelos de regresión lineal ####################
+## Creo que en está sección ya podemos juntar los modelos del ápice y la base
+# Equipo 2: Claudia, Brenda y Angélica, Emmanuel García y Equipo 3: Karla, Mariana, Iván y Karen
+
+
+#Librerias
 library(ggplot2)
 library(corrplot)
 library(Hmisc)
 
 #Cargando la base de datos
-traits <- read.csv("2020_12_31_Base.csv")
+traits <- read.csv("2020_12_31_Base.csv")## Esta base no está en el directorio
 names(traits) #revisar nombres#
 str(traits)
 #######exploratory analyses ######
-
-##### Equipo de la cajita ->
-## Nombres: Cortéz Castro Ericka Belen
-##      Hernández Jessica 
-##			Escalante Pasos Jorge Armín
-##      Martínez  Emmanuel
-##			Dávila Navarro Diego Emilio
 
 #Checar existencia de outliers y corregir el problema
 #tipo de variables que tenemos, que el reconocimiento sea correcto
@@ -187,8 +168,7 @@ colnames(traits.db) <- c("order","family","genus","species",
 
 write.csv(traits.db, "BaseRasgos_UltraCompleta_30dic2020.csv")
 
-#revisamos los outlayers para comprobar que la revision a los valores extraños
-#hayan sido adecuada
+#revisamos los outlayers para comprobar que la revision a los valores extraños hayan sido adecuada
 dotchart(log10(traits.db$long.hoja.consenso))
 hist(log10(traits.db$long.hoja.consenso))
 
@@ -211,7 +191,7 @@ corrplot(long.hoj.matrix$r, type="upper", order="hclust",
          tl.col = "black", tl.srt = 45, pch.cex=2, outline=T, addCoef.col = T)
 
 
-#####Estadística descriptiva. Equipo: Claudia, Brenda y Angélica, Emmanuel García 
+##### Estadística descriptiva. Equipo: Claudia, Brenda y Angélica, Emmanuel García 
 #
 #####P1. ¿Qué tanto explica la longitud de la hoja la variación en 
 ######diámetro  de los vasos en la base de un árbol, con y 
