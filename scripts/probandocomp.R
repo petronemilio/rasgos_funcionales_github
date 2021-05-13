7f419676489654cace3248d28b1a6614ed5a536c
petronemilio <emilio.petrone@st.ib.unam.mx>
2021-05-13 10:12:06 -0500

    Relaimpo comands added.

--- scripts/probandocomp.R
+++ scripts/probandocomp.R
@@ -5,7 +5,7 @@ library(vioplot)
 library(car)
 library(hier.part)
 library(asbio)
-
+library(relaimpo)
 ######Creating a temporal database and removing aphyllous species#####
 traits.temp <- traits.db
 
@@ -29,6 +29,7 @@ summary(lm.vdtip.leaf)
 anova(lm.vdtip.leaf)
 plot(lm.vdtip.leaf)
 confint(lm.vdtip.leaf)
+plot(lm.vdtip.leaf$residuals)
 #
 summary(lm.vdtip.stlplusleaf)
 anova(lm.vdtip.stlplusleaf)
@@ -42,11 +43,24 @@ confint(lm.vdtip.stlintleaf)
 ##4 models. Now compared the ss
 #SS Extra ⫽Full SS Regression ⫺Reduced SS Regression
 anova(lm.vdtip.stl,lm.vdtip.stlintleaf)
-anova(lm.vdtip.stlplusleaf,lm.vdtip.stlintleaf, lm.vdtip.leaf,lm.vdtip.stl)
+anova( lm.vdtip.leaf,lm.vdtip.stl,lm.vdtip.stlplusleaf,lm.vdtip.stlintleaf)
 ####
 hier.part(traits.db$VD.tip.um, traits.db[,c("stem.length.m","unit.leaf.leng")], gof = "Rsqu")
 rand.hp(traits.db$VD.tip.um, traits.db[,c("stem.length.m","unit.leaf.leng")], gof = "Rsqu",
         num.reps = 100)$Iprobs
+####
+
+metrics <- calc.relimp(lm.vdtip.stlplusleaf, type = c("lmg","first", "last","betasq", "pratt"),
+                       rela = TRUE)
+metrics
+boot.vdtip <- boot.relimp(lm.vdtip.stlplusleaf, b = 1000, type = "lmg", bty = "perc",level = 0.95)
+eval.vdtip <- booteval.relimp(boot.vdtip, typesel = c("lmg", "pmvd"), level = 0.9,
+                              bty = "perc", norank = TRUE)
+eval.vdtip
+plot(metrics, names.abbrev = 3)
+plot(booteval.relimp(boot.vdtip, typesel = c("lmg", "pmvd"), level = 0.9),
+     names.abbrev = 2, bty = "perc")
+
 #(SSEreduced - SSEfull) / SSEreduced
 #Partial r2
 partial.R2(lm.vdtip.leaf, lm.vdtip.stlintleaf)
@@ -81,16 +95,39 @@ summary(lm.vdbase.stlintleaf)
 anova(lm.vdbase.stlintleaf)
 plot(lm.vdbase.stlintleaf)
 confint(lm.vdbase.stlintleaf)
+#
+anova( lm.vdbase.leaf,lm.vdbase.stl,lm.vdbase.stlplusleaf,lm.vdbase.stlintleaf)
 ###
 hier.part(traits.db$VD.base.um, traits.db[,c("stem.length.m","unit.leaf.leng")], gof = "Rsqu")
 rand.hp(traits.db$VD.base.um, traits.db[,c("stem.length.m","unit.leaf.leng")], gof = "Rsqu",
         num.reps = 100)$Iprobs
+?rand.hp
 #Partial r2
 partial.R2(lm.vdbase.leaf, lm.vdbase.stlintleaf)
 partial.R2(lm.vdbase.stl, lm.vdbase.stlintleaf)
 partial.R2(lm.vdbase.leaf,lm.vdbase.stlplusleaf)
 partial.R2(lm.vdbase.stl,lm.vdbase.stlplusleaf)
 
+metrics.base <- calc.relimp(lm.vdbase.stlplusleaf, type = c("lmg","first", "last","betasq", "pratt"),
+                       rela = TRUE)
+metrics.base
+boot.vdbase <- boot.relimp(lm.vdbase.stlplusleaf, b = 1000, type = "lmg", bty = "perc",level = 0.95)
+eval.vdbase <- booteval.relimp(boot.vdbase, typesel = c("lmg", "pmvd"), level = 0.9,
+                              bty = "perc", norank = TRUE)
+plot(metrics.base, names.abbrev = 3)
+plot(booteval.relimp(boot.vdbase, typesel = c("lmg", "pmvd"), level = 0.9),
+     names.abbrev = 2, bty = "perc")
+eval.vdtip
+eval.vdbase
+####
+pdf("Results/FigureRelimpvdtip.pdf", height = 8, width = 8) # Para guardar en PDF
+plot(booteval.relimp(boot.vdtip, typesel ="lmg", level = 0.9),
+     names.abbrev = 2, bty = "perc")
+dev.off()
+pdf("Results/FigureRelimpvdbase.pdf", height = 8, width = 8) # Para guardar en PDF
+plot(booteval.relimp(boot.vdbase, typesel = "lmg", level = 0.9),
+     bty = "perc")
+dev.off()
 #####Determine the best model ####
 aic_withformula <- c()
 aic_withformula <- c(aic_withformula ,AIC(lm.vdtip.leaf))
@@ -182,3 +219,26 @@ summary(lm.afilas.base)
 
 lm.afilas.punta <-lm(log10(afilas$VD.tip.um)~ log10(afilas$stem.length.m))
 summary(lm.afilas.punta)
+
+#Running models including the 
+lm.vdtip.stl.withafilas <- lm(log10(traits.db$VD.tip.um) ~ log10(traits.db$stem.length.m))
+lm.vdtip.leaf.withafilas <- lm(log10(traits.db$VD.tip.um) ~ log10(traits.db$unit.leaf.leng+1))
+lm.vdtip.stlplusleaf.withafilas <- lm(log10(traits.db$VD.tip.um) ~ log10(traits.db$stem.length.m)+
+                                        log10(traits.db$unit.leaf.leng+1))
+lm.vdtip.leafintleaf.withafilas <- lm(log10(traits.db$VD.tip.um) ~ log10(traits.db$stem.length.m)*
+                                        log10(traits.db$unit.leaf.leng+1))
+
+####Running models for vdbase
+lm.vdbase.stl.withafilas <- lm(log10(traits.db$VD.base.um) ~ log10(traits.db$stem.length.m))
+lm.vdbase.leaf.withafilas <- lm(log10(traits.db$VD.base.um) ~ log10(traits.db$unit.leaf.leng+1))
+lm.vdbase.stlplusleaf.withafilas <- lm(log10(traits.db$VD.base.um) ~ log10(traits.db$stem.length.m)+
+                                        log10(traits.db$unit.leaf.leng+1))
+lm.vdbase.leafintleaf.withafilas <- lm(log10(traits.db$VD.base.um) ~ log10(traits.db$stem.length.m)*
+                                        log10(traits.db$unit.leaf.leng+1))
+
+######
+summary(lm.vdtip.stl.withafilas)
+summary(lm.vdtip.leafintleaf.withafilas)
+summary(lm.vdtip.stlintleaf)
+summary(lm.vdbase.leafintleaf.withafilas)
+summary(lm.vdbase.stlintleaf)
